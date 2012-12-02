c8e4ef11d597481250a7f88c3b5df3473fc8f13d
Thomas Yeh <tyeh@broadwayedu.com>
2012-12-02 13:32:24 -0800

    commit

--- cowfind.cpp
+++ cowfind.cpp
@@ -3,14 +3,14 @@ ID: tyeh
 LANG: C++
 PROB: cowfind
 */
-#include "stdafx.h"
+//#include "stdafx.h"
 #include <stdio.h>
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <sstream>
 #include <string.h>
-#include <Windows.h>
+//#include <Windows.h>
 
 using namespace std;
 
@@ -26,11 +26,23 @@ int main()
 	ifstream myfile ("cowfind.in");
 	ofstream myout ("cowfind.out");
 	getline(myfile, tmpStr);
+	s= tmpStr.length();
+	cout << s;
+	int answer=0;
 
+	for (int i=0; i<s; i++) {
+		if ((tmpStr[i] == '(') && (tmpStr[i+1] == '(')) 
+			for (int j=i+2; j < s; j++) {
+				if ((tmpStr[j] == ')') && (tmpStr[j+1] == ')'))
+					answer++;
+			}
+	}
+
+	myout << answer << '\n';
 
 	myout.close();
 	myfile.close();
-	
+//	Sleep (10000);
 	return 0;
 }
 
